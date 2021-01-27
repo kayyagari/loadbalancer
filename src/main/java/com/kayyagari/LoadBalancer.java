6dc759464204d8c3c9113673ac88e17c8d2b6145
kayyagari <kayyagari@apache.org>
2021-01-27 23:07:16 +0100

    project structure

---
+++ src/main/java/com/kayyagari/LoadBalancer.java
@@ -0,0 +1,11 @@
+package com.kayyagari;
+
+/**
+ *
+ */
+public class LoadBalancer 
+{
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ src/main/java/com/kayyagari/Provider.java
@@ -0,0 +1,7 @@
+package com.kayyagari;
+
+public interface Provider {
+    String get();
+
+    boolean check();
+}
